75b0bd2dde15fbd2577799e242b1ab539f3a9778
Himanshu0k <hkhimanshu1406@gmail.com>
2024-10-05 11:38:58 +0530

    Linting changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,9 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import pluginJs from "@eslint/js"
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {files: ["*.{js,mjs,cjs,jsx}"]},
   {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  pluginJs.configs.recommended
 ];
